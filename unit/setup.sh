e392834962af1024804d5768f01e2dac0266176d
Tudor Timi <tudortimi@users.noreply.github.com>
2020-12-30 23:50:34 +0100

    Load SVUnit using dependency provisioned by Gradle

--- unit/setup.sh
+++ unit/setup.sh
@@ -1,3 +1,10 @@
-cd ../dependencies/svunit
+cd ..
+./gradlew writeSvunitDirPath
+svunit_dir=$(head -n 1 build/svunit_dir_path.txt)
+cd - > /dev/null
+
+echo "Using SVUnit from $svunit_dir"
+
+cd $svunit_dir
 source Setup.bsh
 cd - > /dev/null
